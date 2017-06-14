8f0bca22b94161ec1bb6a0b25da64e400c68fd00
zenith13 <alwaysparas@gmail.com>
2017-06-14 16:34:53 +0530

    test

---
+++ src/com/test/parent/Demo.java
@@ -0,0 +1,17 @@
+package com.test.parent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by paras on 12-06-2017.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("awedad");
+        List<String> asd=new ArrayList<>();
+        asd.add("asd");
+        asd.forEach(s-> System.out.println(s));
+    }
+}
